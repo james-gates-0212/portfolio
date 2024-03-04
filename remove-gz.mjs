3ff2e55b55b248509aeb7a768646f2d0a8fd535a
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-04 02:47:41 -0800

    fix: remove-gz.mjs for console

--- remove-gz.mjs
+++ remove-gz.mjs
@@ -2,4 +2,5 @@ import { deleteSync } from 'del';
 
 const deletedFilePaths = deleteSync(['./build/**/*.gz']);
 
-console.log('Deleted files:\n', deletedFilePaths.filter(Boolean).join('\n'));
+console.log('Deleted files:');
+console.log(deletedFilePaths.join('\n'));
