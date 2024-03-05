9fc1fc02ac83ada091603ed69776dac8dfe153ac
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-05 04:21:01 -0800

    chore: fix tsparticles engine
    Co-Authored-By: bros0212 <159820590+bros0212@users.noreply.github.com>
    Co-Authored-By: clive-goldminer <161703670+clive-goldminer@users.noreply.github.com>
    Co-Authored-By: koopa0212 <161178569+koopa0212@users.noreply.github.com>
    Co-Authored-By: mario0212 <157266968+mario0212@users.noreply.github.com>

--- src/components/clients/backgrounds/Stars.tsx
+++ src/components/clients/backgrounds/Stars.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { loadFull } from 'tsparticles';
 import { useCallback, useEffect } from 'react';
 import Particles, { initParticlesEngine } from '@tsparticles/react';
+import { loadSlim } from '@tsparticles/slim';
 import type { Container } from '@tsparticles/engine';
 
 export default function Stars() {
@@ -11,13 +11,7 @@ export default function Stars() {
 
   useEffect(() => {
     initParticlesEngine(async (engine) => {
-      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-      // starting from v2 you can add only the features you need reducing the bundle size
-      // await loadAll(engine);
-      await loadFull(engine);
-      // await loadSlim(engine);
-      // await loadBasic(engine);
+      await loadSlim(engine);
     });
   }, []);
 
