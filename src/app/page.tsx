c00ea9a609e291f297f15a8c2c6f45dd4325bf25
james-gates-0212 <james.gates.0212@gmail.com>
2024-03-06 05:54:20 -0800

    fix: layout for cls🚀❤️
    Co-Authored-By: bros0212 <159820590+bros0212@users.noreply.github.com>
    Co-Authored-By: clive-goldminer <161703670+clive-goldminer@users.noreply.github.com>
    Co-Authored-By: koopa0212 <161178569+koopa0212@users.noreply.github.com>
    Co-Authored-By: mario0212 <157266968+mario0212@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,96 +15,102 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <section className="container mx-auto px-2 sm:px-6 lg:px-8">
-      <div className="grid md:grid-cols-12 mt-28">
-        <div className="md:col-span-7 text-4xl flex flex-col items-center justify-center">
-          <div className="pb-4 text-center">
-            <span className={style.wave} role="img" aria-labelledby="wave">
-              👋🏻
-            </span>
-            <br />
-            <br />
-            Hi There!
+    <>
+      <section className="container mx-auto px-2 sm:px-6 lg:px-8 pt-28 min-h-[562px]">
+        <div className="grid md:grid-cols-12">
+          <div className="md:col-span-7 text-4xl flex flex-col items-center justify-center">
+            <div className="pb-4 text-center">
+              <span className={style.wave} role="img" aria-labelledby="wave">
+                👋🏻
+              </span>
+              <br />
+              <br />
+              Hi There!
+            </div>
+
+            <h1 className="py-2 text-center">
+              My name is
+              <strong className="font-bold text-impact"> James Gates</strong>
+            </h1>
+
+            <div className="py-14">
+              <ul className="text-2xl leading-10">
+                <li>
+                  <strong className="text-impact">Senior</strong> Full Stack Developer
+                </li>
+                <li>
+                  <strong className="text-impact">SEO</strong> Expert
+                </li>
+                <li>
+                  Continuously <strong className="text-impact">Learning</strong>
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <h1 className="py-2">
-            My name is
-            <strong className="font-bold text-impact"> James Gates</strong>
-          </h1>
-
-          <div className="py-14">
-            <ul className="text-2xl leading-10">
-              <li>
-                <strong className="text-impact">Senior</strong> Full Stack Developer
-              </li>
-              <li>
-                <strong className="text-impact">SEO</strong> Expert
-              </li>
-              <li>
-                Continuously <strong className="text-impact">Learning</strong>
-              </li>
-            </ul>
+          <div className="md:col-span-5 flex items-center justify-center">
+            <Image
+              src={homeImage}
+              alt="Home"
+              className="w-auto max-h-[450px]"
+              width={505}
+              height={529}
+              title="Home"
+              priority
+            />
           </div>
         </div>
-
-        <div className="md:col-span-5 flex items-center justify-center">
-          <Image
-            src={homeImage}
-            alt="Home"
-            className="w-auto max-h-[450px]"
-            width={505}
-            height={529}
-            title="Home"
-            priority
-          />
-        </div>
-      </div>
-      <div className="grid md:grid-cols-12 mt-28">
-        <div className="md:col-span-8 px-10">
-          <div className="text-4xl uppercase text-center mt-28 mb-16">
-            Let me <span className="text-impact">Introduce</span> myself
+      </section>
+      <section className="container mx-auto px-2 sm:px-6 lg:px-8 pt-28 min-h-[712px]">
+        <div className="grid md:grid-cols-12">
+          <div className="md:col-span-8 px-10">
+            <div className="text-4xl uppercase text-center mb-16">
+              Let me <span className="text-impact">Introduce</span> myself
+            </div>
+            <p className="text-xl leading-8">
+              As an accomplished Senior Full Stack Developer specializing in SEO and MERN Stack development, I offer a
+              proven track record of crafting resilient and intuitive web applications. Proficient in front-end and
+              back-end technologies, I specialize in delivering scalable solutions that elevate user experiences and
+              boost organic traffic through strategic SEO tactics. Leveraging my expertise in the MERN Stack, I create
+              dynamic, responsive applications tailored to meet the evolving demands of contemporary businesses.
+              Committed to continuous learning and staying abreast of cutting-edge technologies, I am adept at
+              delivering pioneering solutions that surpass client expectations.
+              <br />
+              <br />
+              Web Application Development is not only my job, but also my life.
+              <br />
+              <br />I can work full time, 8+ hours a day, 6 days a week. I hope to work with you in a long time.
+            </p>
           </div>
-          <p className="text-xl leading-8">
-            As an accomplished Senior Full Stack Developer specializing in SEO and MERN Stack development, I offer a
-            proven track record of crafting resilient and intuitive web applications. Proficient in front-end and
-            back-end technologies, I specialize in delivering scalable solutions that elevate user experiences and boost
-            organic traffic through strategic SEO tactics. Leveraging my expertise in the MERN Stack, I create dynamic,
-            responsive applications tailored to meet the evolving demands of contemporary businesses. Committed to
-            continuous learning and staying abreast of cutting-edge technologies, I am adept at delivering pioneering
-            solutions that surpass client expectations.
-            <br />
-            <br />
-            Web Application Development is not only my job, but also my life.
-            <br />
-            <br />I can work full time, 8+ hours a day, 6 days a week. I hope to work with you in a long time.
+          <div className="md:col-span-4 flex items-center justify-center px-10 pt-10">
+            <Image src={avatarImage} className="w-auto" alt="Avatar" width="645" height="500" title="Avatar" priority />
+          </div>
+        </div>
+      </section>
+      <section className="container mx-auto px-2 sm:px-6 lg:px-8 min-h-[256px]">
+        <div className="py-14 flex flex-col gap-5 justify-center">
+          <div className="text-4xl uppercase text-center">FIND ME ON</div>
+          <p className="text-center">
+            Feel free to <span className="text-impact">connect</span> with me
           </p>
+          <ul className="home-about-social-links text-center">
+            {LinkInfos.map(({ href, icon: { dark: DarkIcon }, label }, idx) => (
+              <li className={style.socialIcons} key={`intro-social-icon-${idx}`}>
+                <Link
+                  className={classNames(`title-tooltip`, style.homeSocialIcons)}
+                  data-tooltip-content={label}
+                  href={href}
+                  target="_blank"
+                  rel="noreferrer"
+                  aria-label={label}
+                >
+                  <DarkIcon />
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="md:col-span-4 flex items-center justify-center px-10 pt-10">
-          <Image src={avatarImage} className="w-auto" alt="Avatar" width="645" height="500" title="Avatar" priority />
-        </div>
-      </div>
-      <div className="py-14 flex flex-col gap-5 justify-center">
-        <div className="text-4xl uppercase text-center">FIND ME ON</div>
-        <p className="text-center">
-          Feel free to <span className="text-impact">connect</span> with me
-        </p>
-        <ul className="home-about-social-links text-center">
-          {LinkInfos.map(({ href, icon: { dark: DarkIcon }, label }, idx) => (
-            <li className={style.socialIcons} key={`intro-social-icon-${idx}`}>
-              <Link
-                className={classNames(`title-tooltip`, style.homeSocialIcons)}
-                data-tooltip-content={label}
-                href={href}
-                target="_blank"
-                rel="noreferrer"
-                aria-label={label}
-              >
-                <DarkIcon />
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
--- src/components/layouts/RootLayout.tsx
+++ src/components/layouts/RootLayout.tsx
@@ -1,10 +1,19 @@
 import '@/app/globals.css';
 import { GoogleTagManager } from '@next/third-parties/google';
 import { Raleway } from 'next/font/google';
+import { Suspense } from 'react';
 import Footer from '@/components/Footer';
+import Image from 'next/image';
 import NavBar from '@/components/clients/partials/NavBar';
+import PreImage from '@/app/assets/pre.svg';
 import ReactTooltip from '@/components/clients/ReactTooltip';
 import Stars from '@/components/clients/backgrounds/Stars';
+
+const Loading = () => (
+  <div className="fixed top-0 right-0 bottom-0 left-0 flex flex-col items-center justify-center">
+    <Image src={PreImage} alt="Loading..." title="Loading..." priority />
+  </div>
+);
 
 const raleway = Raleway({
   subsets: ['latin'],
@@ -18,12 +27,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={raleway.className}>
-      <body className="home">
-        <Stars />
-        <NavBar />
-        {children}
-        <Footer />
-        <ReactTooltip />
+      <body className="overflow-hidden">
+        <Suspense fallback={<Loading />}>
+          <Stars />
+          <div className="fixed top-16 left-0 right-0 bottom-0 overflow-y-auto overflow-x-hidden">
+            <NavBar />
+            {children}
+            <Footer />
+          </div>
+          <ReactTooltip />
+        </Suspense>
       </body>
       <GoogleTagManager gtmId="G-1R1XC6TD2Z" />
     </html>
